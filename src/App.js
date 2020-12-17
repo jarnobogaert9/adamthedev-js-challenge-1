eb58f8e410c0744bc09dccfaea91e94f46e35247
Jarno Bogaert <jarnobogaert9@gmail.com>
2020-12-17 17:03:21 +0100

    Feat: filter list and display it on page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import JSONPretty from 'react-json-pretty';
+import JSONPrettyMon from 'react-json-pretty/dist/monikai';
 
 function App() {
+  const [transformedData, setTransformedData] = useState({});
+
+  const filterGender = ({ gender, raw }) => {
+    return raw.filter(x => x.gender.toLowerCase() === gender);
+  }
+
+  const filterByLetter = (data) => {
+    const gender = data[0].gender.toLowerCase();
+    // Get every first letter but exclude doubles (this is done automatically by the fact that we are using a Set)
+    let letters = new Set();
+    data.forEach(person => letters.add(person.first_name[0].toLowerCase()))
+
+    // Sort all letters of object into an array
+    const sortedLetters = Array.from(letters).sort((a, b) => {
+      if (a < b) return -1;
+      else if (a > b) return +1;
+      else return 0;
+    })
+
+    // Create key for each letter initialized with an empty array
+    const obj = {};
+    sortedLetters.forEach(letter => obj[letter] = []);
+
+    for (const letter in obj) {
+      const namesByLetter = data.filter(person => person.first_name[0].toLowerCase() === letter);
+      obj[letter] = namesByLetter;
+    }
+    // console.log(obj);
+    return {
+      [gender]: obj
+    }
+  }
+
+  const fetchRawData = async () => {
+    const response = await fetch(`https://raw.githubusercontent.com/machieajones/instagramChallenges/main/dummyUsers.json`).catch(err => console.log(`Error occurred while fetching data: ${err}`));
+    const json = await response.json().catch((err) => console.log(`Error occured while parsin to JSON: ${err}`));
+    return json;
+  }
+
+  const transformData = (raw) => {
+    // Get all males
+    const males = filterGender({ raw, gender: 'male' });
+    // Get all females
+    const females = filterGender({ raw, gender: 'female' });
+
+    // Filter males by letter
+    const malesFilterdByLetter = filterByLetter(males);
+    // Filter females by letter
+    const femalesFilterdByLetter = filterByLetter(females);
+
+    // // Sort 
+    // Object.entries(malesFilterdByLetter).sort((x, y) => {
+    //   if (x.first_name < y.first_name) { return -1; }
+    //   if (x.first_name > y.first_name) { return 1; }
+    //   return 0;
+    // })
+    return {
+      ...malesFilterdByLetter,
+      ...femalesFilterdByLetter
+    }
+  }
+
+  useEffect(() => {
+    // IEFE
+    (async () => {
+      const raw = await fetchRawData();
+      const transformed = transformData(raw);
+      setTransformedData(transformed);
+    })();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1>Adamthedev JS Challenge #1</h1>
+        {transformedData && <JSONPretty id="json-pretty" data={transformedData} theme={JSONPrettyMon}></JSONPretty>}
+      </div>
+    </>
   );
 }
 
