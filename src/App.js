30e211ec236e7813366d5036af0e3587b30be6e9
Jarno Bogaert <jarnobogaert9@gmail.com>
2020-12-17 17:24:11 +0100

    Feat: link to assignment

--- src/App.js
+++ src/App.js
@@ -80,6 +80,7 @@ function App() {
     <>
       <div className="container">
         <h1>Adamthedev JS Challenge #1</h1>
+        <h3><a target="_blank" rel="noreferrer" href="https://www.instagram.com/p/CI5l-3oAJYe/">Assignment description</a></h3>
         <span>GitHub repo: <a target="_blank" rel="noreferrer" href="https://github.com/jarnobogaert9/adamthedev-js-challenge-1">https://github.com/jarnobogaert9/adamthedev-js-challenge-1</a></span>
         {transformedData && <JSONPretty id="json-pretty" data={transformedData} theme={JSONPrettyMon}></JSONPretty>}
       </div>
